4e9c58050201dcbaa69a4631671f8095339e8779
Nguyen Hoang Anh <hoanganh8d@gmail.com>
2023-05-07 14:33:47 +0700

    Change passwordencoder

--- src/main/java/com/hoanganht1k27/springsecuritybasic/config/MyUserDetails.java
+++ src/main/java/com/hoanganht1k27/springsecuritybasic/config/MyUserDetails.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,6 +20,9 @@ import java.util.List;
 public class MyUserDetails implements UserDetailsService {
     @Autowired
     CustomerRepository customerRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
